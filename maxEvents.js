0961bcc99caf12843431c0d7985d6b8e9ae40c60
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2025-07-08 10:17:48 +0300

    8/7/2025

--- maxEvents.js
+++ maxEvents.js
@@ -3,24 +3,7 @@
  * @return {number}
  */
 var maxEvents = function(s) {
-    //  let a={}
-// for(i=0;i<s.length;i++  ){
-//   let frist=s[i][0]
-//   let seconed=s[i][1]
-//   for(j=frist;j<seconed+1;j++){
-//  a[j] = (a[j] || 0) + 1;
-//   }
-// }
-// let res=0
-// if( Object.keys(a).length ===s.length ){
-//   res=s.length
-// }
-// else if (Object.keys(a).length > s.length){
-//  res=s.length
-// }else{
-//   res=Object.keys(a).length
-// }
-     let a={}
+   let a={}
 for(i=0;i<s.length;i++  ){
   let frist=s[i][0]
   let seconed=s[i][1]
@@ -28,14 +11,45 @@ for(i=0;i<s.length;i++  ){
  a[j] = (a[j] || 0) + 1;
   }
 }
-let res=0
-if( Object.keys(a).length ===s.length ){
-  res=s.length
+s.sort((a, b) => a[1] - b[1]);
+let days=Object.keys(a).map((i)=>Number(i));
+let endday=1
+let count =0
+
+for(let i=0;i<s.length;i++){
+ let frist=s[i][0]
+  let seconed=s[i][1]
+  for(j=frist;j<seconed+1;j++){
+  if(days.includes(j)){
+    endday=j
+    count++
+   const valueToRemove = j;
+   const index = days.indexOf(valueToRemove);
+    days.splice(index, 1);
+    break;
+  }
+  }
+
+
+
 }
-else if (Object.keys(a).length > s.length){
- res=s.length
-}else{
-  res=Object.keys(a).length
-}
-return(res);
+return(count);
+};
+var maxEvents2 = function(s) {
+     s.sort((a, b) => a[1] - b[1]);
+
+  let count = 0;
+  let used = new Set(); // to store used days
+
+  for (let [start, end] of s) {
+    for (let day = start; day <= end; day++) {
+      if (!used.has(day)) {
+        used.add(day);
+        count++;
+        break; // move to next interval
+      }
+    }
+  }
+
+  return count;
 };
