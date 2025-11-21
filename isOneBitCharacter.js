454c65149d12271923325fcf694605d56187ade4
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2025-11-21 19:20:30 +0200

    21/11/2025

--- isOneBitCharacter.js
+++ isOneBitCharacter.js
@@ -8,17 +8,19 @@ var isOneBitCharacter = function(bits) {
   return bits.length-1==i;
 };
 var isOneBitCharacter2 = function(bits) {
-let res =true 
-let beforeZero=bits.length-2>0?true:false;
-let beforeIt=bits.length-3>=0?true:false;
-if(beforeZero){
-if(beforeIt){
-    if(bits[bits.length-2]==1 && bits[bits.length-3]==0){
-    res=false;
+ let res =true 
+  for(let i=0;i<bits.length;i++){
+    if (bits[i] ===1){
+      console.log(bits.length,i)
+      let check=i+2 
+      if(check>bits.length-1){
+        console.log(check,bits.length,i);
+        res=false
+        break;
+      }
+      i++;
+    }
   }
-}else res=false;
-
-}
-
-return res;
+  
+return res
 };
