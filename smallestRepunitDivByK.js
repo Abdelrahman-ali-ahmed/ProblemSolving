6bd3a91189ee732accc2262a3d593c2877b7a496
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2025-11-25 16:46:27 +0200

    25/11/2025

--- smallestRepunitDivByK.js
+++ smallestRepunitDivByK.js
@@ -2,6 +2,7 @@
  * @param {number} k
  * @return {number}
  */
+//problem:https://leetcode.com/problems/smallest-integer-divisible-by-k/?envType=daily-question&envId=2025-11-25
 var smallestRepunitDivByK = function(k) {
 if(k%2===0||k%5===0) return -1;
 let r=1%k;
