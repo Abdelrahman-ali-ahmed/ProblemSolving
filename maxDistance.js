ab2a28a4d0a056a47da31786405200aeb528ddf8
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2026-04-20 02:52:10 +0200

    20/4/2026

---
+++ maxDistance.js
@@ -0,0 +1,14 @@
+var maxDistance = function(nums1, nums2) {
+let i = 0, j = 0, max = 0;
+
+while (i < nums1.length && j < nums2.length) {
+  if (nums1[i] <= nums2[j]) {
+    max = Math.max(max, j - i);
+    j++;            
+  } else {
+    i++;            
+  }
+}
+
+return max;
+};
